fc916313347d6d24377554a07472c67976d5daba
sibosop <brian@eastshore.com>
2019-02-19 19:30:27 -0800

    added recusive specs include

--- EventHandler/specs.py
+++ EventHandler/specs.py
@@ -2,20 +2,49 @@
 
 import json
 import Singleton
+import argparse
 
+import os
+defaultSpecPath = os.environ['HOME'] + "/GitProjects/onan/specs/onan.json"
+import sys
 debug=True
 
-def setup(path):
-  global specs
-  
-    
 class specs(object):
   __metaclass__ = Singleton.Singleton
-  
-  
-  def __init__(path):
+
+  def loadSpecs(self,file):
+    if file in self.specList:
+      if debug: print "%s already loaded"%file
+      return
+    self.specList.append(file)
+    path = "%s/%s"%(self.specDir,file)
+    if debug: print "path %s"%path
+    specs = {}
     with open(path) as f:
-      self.specs = json.load(f)
+      specs = json.load(f)
+      if 'include' in specs:
+        if debug: print "found include"
+        for i in specs['include']:
+          self.loadSpecs(i)
+    self.specs.update(specs)
+  
+  def __init__(self,path):
+    self.specDir = os.path.dirname(path)
+    file = os.path.basename(path)
+    self.specs = {}
+    self.specList = []
+    self.loadSpecs(file)
       
-  def specs():
-    return self.specs
+  def attr(self,a):
+    return self.specs[a];
+
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser() 
+  parser.add_argument('-c','--config',nargs=1,type=str,default=[defaultSpecPath],help='specify different config file')
+  args = parser.parse_args()
+  if debug: print("config path"+args.config[0])
+  specs = specs(args.config[0])
+  print specs.specs
+
